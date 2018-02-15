2cf303f6fb5ec7360f5f6023316eeb388705066b
tigrank <tigrank05@gmail.com>
2018-02-15 17:09:29 +0300

    Написан метод расчета цены на нефтебазе на основе цены на границе

--- Delivery Calculator/CalculatorViewController.swift
+++ Delivery Calculator/CalculatorViewController.swift
@@ -10,33 +10,43 @@ import UIKit
 
 class CalculatorViewController: UIViewController, URLSessionDataDelegate {
     var dataModel: Model? = nil
+    var petrol: String = "АИ-80"
     
-    func downloadRate() {
-        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")
-        let defaultSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-        let dataTask = defaultSession.dataTask(with: url!)
-        dataTask.resume()
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        var response: Object?
-        var rate: Double? = nil
-        do {
-            response = try JSONDecoder().decode(Object.self, from: data)
-        } catch let error {
-            print("JSONDecoder error: " + error.localizedDescription + "/n")
-            return
-        }
-        guard let kgs = response?.Valute["KGS"],
-        let usd = response?.Valute["USD"] else {
-            print("Dictionary does not contain results key/n")
-            return
-        }
-        rate = Double(Int((usd.Value / (kgs.Value/Double(kgs.Nominal))) * 10000)) / 10000
-        self.dataModel = Model(exchangeRate: rate)
-    }
-
+    @IBOutlet weak var borderPrice: UITextField!
+    @IBOutlet weak var petrolPicker: UIPickerView!
+    @IBOutlet weak var baseResults: UILabel!
     @IBOutlet weak var calculatorScrollView: UIScrollView!
+    @IBAction func baseCalculationButton(_ sender: Any) {
+        let borderPriceUSD = Double(borderPrice.text!)
+        let borderPriceKGS = borderPriceUSD! * (dataModel?.exchangeRate)!
+        var duty: Double = 0
+        if petrol == "ДТ" {
+            duty = (dataModel?.dieselDuty)!
+        } else {
+            duty = (dataModel?.petrolDuty)!
+        }
+        let eco = (dataModel?.ecologicalRate)!
+        let priceAfterVat = (borderPriceKGS + duty + eco) * (1.0 + ((dataModel?.vat)! / 100))
+        let railRate = (dataModel?.railwayRate)! * (dataModel?.exchangeRate)!
+        let autoRate = (dataModel?.autoRate)! / 59.0
+        let elnurRate = (dataModel?.elnurRate)! / 59.0
+        var density: Double = 0
+        switch petrol {
+        case "АИ-80":
+            density = 0.715
+        case "АИ-92":
+            density = 0.735
+        case "АИ-95":
+            density = 0.733
+        default:
+            density = 0.855
+        }
+        let liter = 1000 / density
+        let totalPrice = (priceAfterVat + railRate + autoRate + elnurRate) / liter
+        self.baseResults.text = String(totalPrice)
+    }
+    
+    var pickerData = [String]()
     
     @objc func keyboardWasShown(notification: Notification) {
         let info = notification.userInfo! as NSDictionary
@@ -59,8 +69,12 @@ class CalculatorViewController: UIViewController, URLSessionDataDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.petrolPicker.delegate = self
+        self.petrolPicker.dataSource = self
         
         self.downloadRate()
+        
+        pickerData = ["АИ-80", "АИ-92", "АИ-95", "ДТ"]
         
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboard))
         calculatorScrollView?.addGestureRecognizer(hideKeyboardGesture)
@@ -84,5 +98,57 @@ class CalculatorViewController: UIViewController, URLSessionDataDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func downloadRate() {
+        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")
+        let defaultSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let dataTask = defaultSession.dataTask(with: url!)
+        dataTask.resume()
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        var response: Object?
+        var rate: Double? = nil
+        do {
+            response = try JSONDecoder().decode(Object.self, from: data)
+        } catch let error {
+            print("JSONDecoder error: " + error.localizedDescription + "/n")
+            return
+        }
+        guard let kgs = response?.Valute["KGS"],
+            let usd = response?.Valute["USD"] else {
+                print("Dictionary does not contain results key/n")
+                return
+        }
+        rate = Double(Int((usd.Value / (kgs.Value/Double(kgs.Nominal))) * 10000)) / 10000
+        self.dataModel = Model(exchangeRate: rate)
+    }
+}
+
+extension CalculatorViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            petrol = "АИ-80"
+        case 1:
+            petrol = "АИ-92"
+        case 2:
+            petrol = "АИ-95"
+        default:
+            petrol = "ДТ"
+        }
     }
 }
