af52c86d7dddfda7bfadf4b0e4eb3d25a0cbee36
tigrank <tigrank05@gmail.com>
2018-02-15 09:59:51 +0300

    Добавлен протокол делегата

---
+++ Delivery Calculator/QueryServiceDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  QueryServiceDelegate.swift
+//  Delivery Calculator
+//
+//  Created by Тигран on 14.02.2018.
+//  Copyright © 2018 PetrovectorGroup. All rights reserved.
+//
+
+import Foundation
+
+protocol QueryServiceDelegate: class {
+    func didFinishDownloadingRates(sender: QueryService)
+}
