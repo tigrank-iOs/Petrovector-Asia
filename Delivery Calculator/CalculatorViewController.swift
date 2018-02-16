e7785ff36086b3295603f74f1cf7ff9c7b65eda3
tigrank <tigrank05@gmail.com>
2018-02-16 13:04:11 +0300

    Загрузка из сети, pickerView вынесены в отдельный файл с помощью расширений

--- Delivery Calculator/CalculatorViewController.swift
+++ Delivery Calculator/CalculatorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController, URLSessionDataDelegate {
+class CalculatorViewController: UIViewController {
     var dataModel: Model? = nil
     var petrol: String = "АИ-80"
     
@@ -98,57 +98,5 @@ class CalculatorViewController: UIViewController, URLSessionDataDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    func downloadRate() {
-        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")
-        let defaultSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-        let dataTask = defaultSession.dataTask(with: url!)
-        dataTask.resume()
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        var response: Object?
-        var rate: Double? = nil
-        do {
-            response = try JSONDecoder().decode(Object.self, from: data)
-        } catch let error {
-            print("JSONDecoder error: " + error.localizedDescription + "/n")
-            return
-        }
-        guard let kgs = response?.Valute["KGS"],
-            let usd = response?.Valute["USD"] else {
-                print("Dictionary does not contain results key/n")
-                return
-        }
-        rate = Double(Int((usd.Value / (kgs.Value/Double(kgs.Nominal))) * 10000)) / 10000
-        self.dataModel = Model(exchangeRate: rate)
-    }
-}
-
-extension CalculatorViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.pickerData.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.pickerData[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        switch row {
-        case 0:
-            petrol = "АИ-80"
-        case 1:
-            petrol = "АИ-92"
-        case 2:
-            petrol = "АИ-95"
-        default:
-            petrol = "ДТ"
-        }
     }
 }
---
+++ Delivery Calculator/Extensions.swift
@@ -0,0 +1,77 @@
+//
+//  Extensions.swift
+//  Delivery Calculator
+//
+//  Created by Тигран on 16.02.2018.
+//  Copyright © 2018 PetrovectorGroup. All rights reserved.
+//
+
+import UIKit
+
+extension CalculatorViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            petrol = "АИ-80"
+        case 1:
+            petrol = "АИ-92"
+        case 2:
+            petrol = "АИ-95"
+        default:
+            petrol = "ДТ"
+        }
+    }
+}
+
+extension CalculatorViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.baseCalculationButton(self)
+        return true
+    }
+}
+
+extension CalculatorViewController: URLSessionDataDelegate {
+    func downloadRate() {
+        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")
+        let defaultSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        let dataTask = defaultSession.dataTask(with: url!)
+        dataTask.resume()
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        var response: Object?
+        var rate: Double? = nil
+        do {
+            response = try JSONDecoder().decode(Object.self, from: data)
+        } catch let error {
+            print("JSONDecoder error: " + error.localizedDescription + "/n")
+            return
+        }
+        guard let kgs = response?.Valute["KGS"],
+            let usd = response?.Valute["USD"] else {
+                print("Dictionary does not contain results key/n")
+                return
+        }
+        rate = Double(Int((usd.Value / (kgs.Value/Double(kgs.Nominal))) * 10000)) / 10000
+        self.dataModel = Model(exchangeRate: rate)
+        
+//        let tabBar = self.tabBarController?.viewControllers
+//        let settingVC = tabBar![1] as! SettingsVC
+//        settingVC.dataModel = self.dataModel
+//        
+//        let borderVC = tabBar![2] as! BorderCalculatorVC
+//        borderVC.dataModel = self.dataModel
+    }
+}
