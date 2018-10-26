92632152bd65df8f192e34d811a8ebb0c80c8323
tigrank <tigrank05@gmail.com>
2018-10-26 13:02:59 +0300

    Добавил возможность удаления прогноза

--- Delivery Calculator/App/Business Logic/CoreDataManager.swift
+++ Delivery Calculator/App/Business Logic/CoreDataManager.swift
@@ -21,17 +21,6 @@ public class CoreDataManager {
 		let container = NSPersistentContainer(name: "DataModel")
 		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			if let error = error as NSError? {
-				// Replace this implementation with code to handle the error appropriately.
-				// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-				
-				/*
-				Typical reasons for an error here include:
-				* The parent directory does not exist, cannot be created, or disallows writing.
-				* The persistent store is not accessible, due to permissions or data protection when the device is locked.
-				* The device is out of space.
-				* The store could not be migrated to the current model version.
-				Check the error message to determine what the actual problem was.
-				*/
 				fatalError("Unresolved error \(error), \(error.userInfo)")
 			}
 		})
@@ -48,6 +37,12 @@ public class CoreDataManager {
 				fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
 			}
 		}
+	}
+	
+	public func delete(_ object: NSManagedObject) {
+		let context = persistentContainer.viewContext
+		context.delete(object)
+		
 	}
 	
 	public func entityForName(entityName: String) -> NSEntityDescription {
---
+++ Delivery Calculator/App/Flows/Forecast/ForecastReportVC.swift
@@ -0,0 +1,91 @@
+//
+//  ForecastReportVC.swift
+//  Delivery Calculator
+//
+//  Created by Тигран on 28/08/2018.
+//  Copyright © 2018 PetrovectorGroup. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ForecastReportVC: UIViewController {
+
+	// MARK: - Variables
+	var countModel: CountModel!
+	private let myPointAnnotation = MKPointAnnotation()
+	
+	// MARK: - Outlets
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var azsTypeLabel: UILabel!
+	@IBOutlet weak var dayLabel: UILabel!
+	@IBOutlet weak var passedLabel: UILabel!
+	@IBOutlet weak var enteredLabel: UILabel!
+	@IBOutlet weak var ai80Label: UILabel!
+	@IBOutlet weak var ai92Label: UILabel!
+	@IBOutlet weak var ai95Label: UILabel!
+	@IBOutlet weak var dtLabel: UILabel!
+	@IBOutlet weak var conversionLabel: UILabel!
+	
+	@IBOutlet weak var mapView: MKMapView!
+	
+	// MARK: - VCLifeCycle
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		setData()
+		setMap()
+    }
+	
+	// MARK: - Private Functions
+	private func setData() {
+		self.nameLabel.text = countModel.name
+		self.azsTypeLabel.text = countModel.azsType
+		self.dayLabel.text = countModel.dayOfWeek
+		self.passedLabel.text = countModel.passedCars.description
+		self.enteredLabel.text = countModel.enterCars.description
+		
+		let forecasts = countModel.forecasts?.allObjects as! [Forecast]
+		for forecast in forecasts {
+			switch forecast.fuel {
+			case 3: self.ai80Label.text = forecast.value.description
+			case 4: self.ai92Label.text = forecast.value.description
+			case 1: self.ai95Label.text = forecast.value.description
+			case 7: self.dtLabel.text = forecast.value.description
+			default: break
+			}
+		}
+		self.conversionLabel.text = countModel.conversion.description
+	}
+	
+	private func setMap() {
+		let coordinates = CLLocationCoordinate2DMake(CLLocationDegrees(countModel.latitude), CLLocationDegrees(countModel.longitude))
+		let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 500, longitudinalMeters: 500)
+		mapView.setRegion(region, animated: false)
+		mapView.delegate = self
+		myPointAnnotation.coordinate = coordinates
+		mapView.addAnnotation(myPointAnnotation)
+		
+		let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+		let coder = CLGeocoder()
+		coder.reverseGeocodeLocation(location) { [weak self] places, _ in
+			guard let strongSelf = self else { return }
+			if let selectedPlace = places?.first {
+				
+				guard let street = selectedPlace.addressDictionary?["Street"] as? String,
+					let city = selectedPlace.addressDictionary?["City"] as? String else { return }
+				
+				let fullAdress = "\(city), \(street)"
+				strongSelf.myPointAnnotation.title = fullAdress
+			}
+		}
+	}
+}
+
+extension ForecastReportVC: MKMapViewDelegate {
+	
+	// MARK: - MKMapViewDelegate
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		return MKTileOverlayRenderer(overlay: overlay)
+	}
+}
